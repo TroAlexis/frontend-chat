db23a18d152b8c485bd9e65fcc98380667fc2f89
TroAlexis <aleksei.troshin1997@gmail.com>
2020-10-13 22:38:59 +0300

    External className possible for info-section.

--- src/js/components/info-section/info-section.jsx
+++ src/js/components/info-section/info-section.jsx
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 
 import './info-section.scss';
 
-const InfoSection = ({ title, paragraph }) => pug`
-  .info-section
-    h1.info-section__title= title
+const InfoSection = ({ title, paragraph, className }) => pug`
+  .info-section(className=className)
+    h1.info-section__title=title
     p.info-section__paragraph=paragraph
 `;
 
 InfoSection.propTypes = {
   title: PropTypes.string,
   paragraph: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export default InfoSection;
