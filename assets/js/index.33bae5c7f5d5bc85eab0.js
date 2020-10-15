333e3aa2c957a07343fde6068d95f85e1567efbb
TroAlexis <aleksei.troshin1997@gmail.com>
2020-10-15 15:53:09 +0300

    Updates

---
+++ assets/js/index.33bae5c7f5d5bc85eab0.js
@@ -0,0 +1,53 @@
+!function(e){function t(t){for(var n,u,i=t[0],c=t[1],f=t[2],p=0,l=[];p<i.length;p++)u=i[p],Object.prototype.hasOwnProperty.call(o,u)&&o[u]&&l.push(o[u][0]),o[u]=0;for(n in c)Object.prototype.hasOwnProperty.call(c,n)&&(e[n]=c[n]);for(s&&s(t);l.length;)l.shift()();return a.push.apply(a,f||[]),r()}function r(){for(var e,t=0;t<a.length;t++){for(var r=a[t],n=!0,i=1;i<r.length;i++){var c=r[i];0!==o[c]&&(n=!1)}n&&(a.splice(t--,1),e=u(u.s=r[0]))}return e}var n={},o={1:0},a=[];function u(t){if(n[t])return n[t].exports;var r=n[t]={i:t,l:!1,exports:{}};return e[t].call(r.exports,r,r.exports,u),r.l=!0,r.exports}u.m=e,u.c=n,u.d=function(e,t,r){u.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},u.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},u.t=function(e,t){if(1&t&&(e=u(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(u.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var n in e)u.d(r,n,function(t){return e[t]}.bind(null,n));return r},u.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return u.d(t,"a",t),t},u.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},u.p="/frontend-chat/";var i=window.webpackJsonp=window.webpackJsonp||[],c=i.push.bind(i);i.push=t,i=i.slice();for(var f=0;f<i.length;f++)t(i[f]);var s=c;a.push([12,2,0]),r()}([,,,,,,,,
+/*!****************************************!*\
+  !*** ./src/assets/img/arrow-right.svg ***!
+  \****************************************/
+/*! exports provided: default, ReactComponent */
+/*! exports used: ReactComponent */,function(e,t,r){"use strict";r.d(t,"a",(function(){return u}));var n=r(/*! react */0);function o(){return(o=Object.assign||function(e){for(var t=1;t<arguments.length;t++){var r=arguments[t];for(var n in r)Object.prototype.hasOwnProperty.call(r,n)&&(e[n]=r[n])}return e}).apply(this,arguments)}var a=n.createElement("path",{fill:"#fff",d:"M94.35 0l-35.7 35.7L175.95 153 58.65 270.3l35.7 35.7 153-153z"});function u(e){return n.createElement("svg",o({width:"40%",height:"40%",viewBox:"0 0 306 306"},e),a)}r.p},,
+/*!************************************!*\
+  !*** ./src/entries/index/index.js ***!
+  \************************************/
+/*! no exports provided */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is an entry point */,function(e,t,r){"use strict";r.r(t);var n=r(/*! react */0),o=r.n(n),a=r(/*! react-dom */4),u=r.n(a),i=(r(/*! ../../assets/scss/global.scss */17),r(/*! Components/App/App */10));var c=document.querySelector(".root");u.a.render(o.a.createElement(i.a,null),c)},,,,
+/*!*************************************!*\
+  !*** ./src/assets/scss/global.scss ***!
+  \*************************************/
+/*! no static exports found */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */,function(e,t,r){},,,,,,
+/*!*****************************************************!*\
+  !*** ./src/assets/img/userIcons sync ^\.\/.*\.jpg$ ***!
+  \*****************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */,function(e,t,r){var n={"./avatar.jpg":25};function o(e){var t=a(e);return r(t)}function a(e){if(!r.o(n,e)){var t=new Error("Cannot find module '"+e+"'");throw t.code="MODULE_NOT_FOUND",t}return n[e]}o.keys=function(){return Object.keys(n)},o.resolve=a,e.exports=o,o.id=24},
+/*!*********************************************!*\
+  !*** ./src/assets/img/userIcons/avatar.jpg ***!
+  \*********************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t,r){e.exports=r.p+"assets/images/avatar-83aaa0bcc6d7fc00b04967f10d6e8ae5.jpg"},,
+/*!*******************************************************!*\
+  !*** ./src/assets/img/attachments sync ^\.\/.*\.jpg$ ***!
+  \*******************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */,function(e,t,r){var n={"./dog-image-1.jpg":29,"./dog-image-2.jpg":30,"./dog-image-3.jpg":31};function o(e){var t=a(e);return r(t)}function a(e){if(!r.o(n,e)){var t=new Error("Cannot find module '"+e+"'");throw t.code="MODULE_NOT_FOUND",t}return n[e]}o.keys=function(){return Object.keys(n)},o.resolve=a,e.exports=o,o.id=28},
+/*!****************************************************!*\
+  !*** ./src/assets/img/attachments/dog-image-1.jpg ***!
+  \****************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t,r){e.exports=r.p+"assets/images/dog-image-1-1b25be65987bfb5489a70f1e879ae0d9.jpg"},
+/*!****************************************************!*\
+  !*** ./src/assets/img/attachments/dog-image-2.jpg ***!
+  \****************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t,r){e.exports=r.p+"assets/images/dog-image-2-ad6b5f8ed50c932aeed22c329e881857.jpg"},
+/*!****************************************************!*\
+  !*** ./src/assets/img/attachments/dog-image-3.jpg ***!
+  \****************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t,r){e.exports=r.p+"assets/images/dog-image-3-c58ef7b46094804e64093504ce0093c6.jpg"}]);
